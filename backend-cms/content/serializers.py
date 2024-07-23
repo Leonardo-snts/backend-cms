c39698b745701b2f7c30e2e01fcd9cc88f576710
Leonardo-snts <leonardosantos.ms77@gmail.com>
2024-07-22 22:37:28 -0400

    Foi criado uma pagina para exibição das imagens e dos documentos

--- backend-cms/content/serializers.py
+++ backend-cms/content/serializers.py
@@ -1,11 +1,11 @@
+# content/serializers.py
 from rest_framework import serializers
 from .models import Article, Image, Document
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        #fields = '__all__'
-        fields = ['id', 'title', 'content', 'created_at', 'template']
+        fields = ['id', 'title', 'content', 'template']
 
 class ImageSerializer(serializers.ModelSerializer):
     image_url = serializers.SerializerMethodField()
@@ -15,7 +15,7 @@ class ImageSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'image', 'image_url']
 
     def get_image_url(self, obj):
-        return obj.image.url
+        return self.context['request'].build_absolute_uri(obj.image.url)
 
 class DocumentSerializer(serializers.ModelSerializer):
     document_url = serializers.SerializerMethodField()
@@ -25,4 +25,4 @@ class DocumentSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'document', 'document_url']
 
     def get_document_url(self, obj):
-        return obj.document.url
+        return self.context['request'].build_absolute_uri(obj.document.url)
